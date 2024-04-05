42961ca39fbdd7f4c4b7126c616b832039a4eb7a
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-04-05 16:53:47 +0200

    designing search engine

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,18 @@
+import styles from '../../ui/dashboard/users/users.module.css'
+import Search from '../../ui/dashboard/search/search'
+import Link from 'next/link'
+
 const UsersPage = () => {
   return (
-    <div>
-      <h1>Userlist</h1>
+    <div className={styles.container}>
+        <div className={styles.top}>
+          <Search  placeholder="Search for an user" />
+          <Link href="/dashboard/users/add" />
+          <button className={styles.add}>Add User</button>
+        </div>
+        <table className={styles.table}>
+
+        </table>
     </div>
   );
 }
