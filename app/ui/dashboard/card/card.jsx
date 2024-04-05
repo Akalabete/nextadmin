3d45bde6f1697db1fba03edaf8c76c62d88ba134
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-04-05 10:01:07 +0200

    added a variable to the css, and finished card design

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,11 +1,17 @@
 import styles from './card.module.css';
-
+import {MdSupervisedUserCircle} from 'react-icons/md';
 const Card = () => {
     
     return (
         <div className={styles.container}>
-            
-            card
+           <MdSupervisedUserCircle size={24}  />
+           <div className={styles.texts}>
+                <span className={styles.title}>Total Users</span>
+                <span className={styles.number}>12.345</span>
+                <span className={styles.details}>
+                    <span className={styles.positive}>12%</span> more than previously
+                </span>
+           </div>
         </div>
         )
     }
