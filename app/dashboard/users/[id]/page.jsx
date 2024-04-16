f641bf8fbc8bd290edaee023d56fb7e565ad133c
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-04-16 16:12:29 +0200

    user profile page designed

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -12,54 +12,56 @@ const SingleUserPage = () => {
                 John Doe
             </div>
             <div className={styles.formContainer}>
-                <label>Username</label>
-                <input 
-                    type="text"
-                    name="username"
-                    placeholder="John Doe"
-                />
-                <label>Email</label>
-                <input 
-                    type="email"
-                    name="email"
-                    placeholder="JohnDoe@test.fr"
-                />
-                <label>Email</label>
-                <input 
-                    type="email"
-                    name="email"
-                    placeholder="JohnDoe@test.fr"
-                />
-                <label>Password</label>
-                <input 
-                    type="password"
-                    name="password"
-                    placeholder="********"
-                />
-                <label>Phone</label>
-                <input 
-                    type="phone"
-                    name="phone"
-                    placeholder="1234567890"
-                />
-                <label>Adress</label>
-                <textarea
-                    id="adress"
-                    name="adress"
-                    placeholder="adress"
-                >
-                </textarea>
-                <label>Is Admin?</label>
-                <select name="isadmin">
-                    <option value="true">Yes</option>
-                    <option value="false">No</option>
-                </select>
-                <label>Is Active</label>
-                <select name="isactive">
-                    <option value="true">Yes</option>
-                    <option value="false">No</option>
-                </select>
-                <button className={styles.submitBtn}>Edit Profile</button>
+                <form action="" className={styles.form}>
+                    <label>Username</label>
+                    <input 
+                        type="text"
+                        name="username"
+                        placeholder="John Doe"
+                    />
+                    <label>Email</label>
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="JohnDoe@test.fr"
+                    />
+                    <label>Email</label>
+                    <input 
+                        type="email"
+                        name="email"
+                        placeholder="JohnDoe@test.fr"
+                    />
+                    <label>Password</label>
+                    <input 
+                        type="password"
+                        name="password"
+                        placeholder=""
+                    />
+                    <label>Phone</label>
+                    <input 
+                        type="phone"
+                        name="phone"
+                        placeholder="1234567890"
+                    />
+                    <label>Adress</label>
+                    <textarea
+                        id="adress"
+                        name="adress"
+                        placeholder="adress"
+                    >
+                    </textarea>
+                    <label>Is Admin?</label>
+                    <select name="isadmin">
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select>
+                    <label>Is Active</label>
+                    <select name="isactive">
+                        <option value="true">Yes</option>
+                        <option value="false">No</option>
+                    </select>
+                    <button>Update Profile</button>
+                </form>
             </div>
         </div>
     );
