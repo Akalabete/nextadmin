8fad7392f05017dc67294041feba8a722f0e59a8
LACOUR Alexandre <lacour_alexandre@proton.me>
2024-04-23 14:27:56 +0200

    encrypt user password upon inscription

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,22 +1,28 @@
 import { User } from "./models";
 import { connectToDatabase } from "./utils";
 import { redirect } from "next/navigation";
-import { revalidatePath } from 'next/cache'
+import { revalidatePath } from 'next/cache';
+import bcrypt from 'bcrypt';
+
 export const addUser = async (formData) => {
     "use server";
+    const varsalt = parseInt(process.env.SALT);
     const { username, email, password, phone, adress, img, isAdmin, isActive } = Object.fromEntries(formData);
     try {
         connectToDatabase();
+        const salt = await bcrypt.genSalt(varsalt);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new User ({
             username,
             email,
-            password,
+            password: hashedPassword,
             phone,
             adress,
             img,
             isAdmin,
             isActive
         });
+        
         await newUser.save();
 
     }catch(err) {
